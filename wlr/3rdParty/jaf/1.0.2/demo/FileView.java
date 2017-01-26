2ce5c94dd0e47ad9365a021051ae858e37be3074
XiaoyongYang <xiaoyongyang5656@gmail.com>
2017-01-26 08:58:47 +0100

    Part of code of Tele2 project

---
+++ wlr/3rdParty/jaf/1.0.2/demo/FileView.java
@@ -0,0 +1,38 @@
+
+import java.awt.*;
+import java.beans.*;
+import java.net.*;
+import javax.activation.*;
+
+public class FileView {
+    private Frame frame;
+
+    public static void main(String args[]) throws Exception {
+        FileView fv = new FileView();
+        if (args.length == 0) {
+            System.out.println("usage: FileView  file.txt");
+            System.exit(1);
+        }
+        fv.view(args[0]);
+    }
+
+    private void view(String filename) throws Exception {
+  	FileDataSource fds = new FileDataSource(filename); 
+  	DataHandler dh = new DataHandler(fds); 
+	// comment out previous two lines, and uncomment next
+	// line and pass in a URL on the command line.
+	// DataHandler dh = new DataHandler(new URL(filename));
+
+	CommandInfo bi = dh.getCommand("view");
+	
+	if (bi == null) {
+	    System.out.println("no viewer found, exiting");
+	    System.exit(1);
+	}
+
+	frame = new Frame("Viewer");
+	frame.add((Component)dh.getBean(bi));
+	frame.setSize(new Dimension(400,300));
+	frame.show();
+    }
+}
---
+++ wlr/3rdParty/jaf/1.0.2/demo/PrefTest.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.beans.*;
+import javax.activation.*;
+
+public class PrefTest {
+
+    public static void main(String args[]) {
+	MailcapCommandMap mcf = null;
+
+	if (args.length > 0) {
+	    try {
+		mcf = new MailcapCommandMap(args[0]);
+	    } catch (Exception e) {
+		e.printStackTrace();
+		System.exit(1);
+	    }
+	} else
+	    mcf = new MailcapCommandMap();
+
+	CommandInfo cmdinfo[] = mcf.getAllCommands("text/plain");
+	
+	if (cmdinfo != null) {
+	    System.out.println("ALL Commands for text/plain:");
+	    for (int i = 0; i < cmdinfo.length; i++) {
+		System.out.println("Verb: " + cmdinfo[i].getCommandName() +
+				  " Class: " + cmdinfo[i].getCommandClass());
+	    }
+	    System.out.println("done");
+	} else {
+	    System.out.println("no commands");
+	}
+	System.out.println();
+
+	cmdinfo = mcf.getPreferredCommands("text/plain");
+	if (cmdinfo != null) {
+	    System.out.println("PREFERRED Commands for text/plain:");
+	    for (int i = 0; i < cmdinfo.length; i++) {
+		System.out.println("Verb: " + cmdinfo[i].getCommandName() +
+				  " Class: " + cmdinfo[i].getCommandClass());
+	    }
+	    System.out.println("done");
+	} else {
+	    System.out.println("no commands");
+	}
+	System.out.println();
+    }
+}
